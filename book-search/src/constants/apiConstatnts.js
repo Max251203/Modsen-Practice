30e9b0570143dea1798c3b1a5bd366cedd943724
MaksMurashko <maksmd251203@gmail.com>
2024-07-04 11:37:37 +0300

    fetch and books object moved to constants

--- book-search/src/constants/apiConstatnts.js
+++ book-search/src/constants/apiConstatnts.js
@@ -1,2 +1,25 @@
+import { API_KEY, BASE_URL } from "constants/apiConstatnts";
+
 export const API_KEY = "AIzaSyCI_B28-8QZokqkrYuq-nwnG1NzXL9E6g8";
 export const BASE_URL = "https://www.googleapis.com/books/v1/volumes";
+
+export const REQUEST = (searchParams, startIndex) => {
+    return `${BASE_URL}?q=${searchParams.searchTerm}${
+      searchParams.category !== "all"
+        ? `+subject:${searchParams.category}`
+        : ""
+    }&orderBy=${
+      searchParams.sortBy
+    }&startIndex=${startIndex}&maxResults=30&key=${API_KEY}`;
+};
+  
+export const MAPPED_BOOKS = (items) => {
+    return items.map((book) => ({
+      id: book.id,
+      title: book.volumeInfo.title,
+      authors: book.volumeInfo.authors || [],
+      categories: book.volumeInfo.categories || [],
+      thumbnail: book.volumeInfo.imageLinks?.thumbnail || '',
+      description: book.volumeInfo.description || '',
+    }));
+  };
--- book-search/src/context.jsx
+++ book-search/src/context.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useCallback } from "react";
-import { API_KEY, BASE_URL } from "constants/apiConstatnts";
+import { REQUEST, MAPPED_BOOKS } from "constants/apiConstatnts";
 
 const AppContext = React.createContext();
 
@@ -18,29 +18,12 @@ const AppProvider = ({ children }) => {
   const fetchBooks = useCallback(async () => {
     setLoading(true);
     try {
-      const response = await fetch(
-        `${BASE_URL}?q=${searchParams.searchTerm}${
-          searchParams.category !== "all"
-            ? `+subject:${searchParams.category}`
-            : ""
-        }
-        &orderBy=${
-          searchParams.sortBy
-        }&startIndex=${startIndex}&maxResults=30&key=${API_KEY}`
-      );
+      const response = await fetch(REQUEST(searchParams, startIndex));
       const data = await response.json();
       const { items, totalItems: total } = data;
 
       if (items) {
-        const newBooks = items.map((book) => ({
-          id: book.id,
-          title: book.volumeInfo.title,
-          authors: book.volumeInfo.authors || [],
-          categories: book.volumeInfo.categories || [],
-          thumbnail: book.volumeInfo.imageLinks?.thumbnail || "",
-          description: book.volumeInfo.description || "",
-        }));
-
+        const newBooks = MAPPED_BOOKS(items);
         setBooks((prevBooks) => [...prevBooks, ...newBooks]);
         setTotalItems(total);
       }
