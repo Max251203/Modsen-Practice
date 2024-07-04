4d0727cc29fab247ad402e25f7af9e64b031cd00
MaksMurashko <maksmd251203@gmail.com>
2024-07-04 11:39:56 +0300

    fix

--- book-search/src/constants/apiConstatnts.js
+++ book-search/src/constants/apiConstatnts.js
@@ -1,5 +1,3 @@
-import { API_KEY, BASE_URL } from "constants/apiConstatnts";
-
 export const API_KEY = "AIzaSyCI_B28-8QZokqkrYuq-nwnG1NzXL9E6g8";
 export const BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 
